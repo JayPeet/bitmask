47bfdffcc279a9756e0179bc4434444ca6893c18
Jay X Peet <jaytechmedia@googlemail.com>
2022-05-18 23:22:04 +0100

    Fix github path in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bitmask.MixProject do
   def project do
     [
       app: :bitmask,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       deps: deps(),
 
@@ -39,7 +39,7 @@ defmodule Bitmask.MixProject do
     [
       files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/jaypeet/bitmask-elixir"}
+      links: %{"GitHub" => "https://github.com/jaypeet/bitmask"}
     ]
   end
 end
